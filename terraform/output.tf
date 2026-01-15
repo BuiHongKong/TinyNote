dbe2c76838dd23d2698e571c853b3bbd992f3492
Kong <rd_kong@bpo-it.net>
2026-01-15 15:18:18 +0700

    setup infra

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,5 @@
-output "app_runner_url" {
-  value = aws_apprunner_service.tinynote.service_url
-}
+# Tạm thời comment vì App Runner service chưa được tạo
+# Uncomment sau khi App Runner service được tạo
+# output "app_runner_url" {
+#   value = aws_apprunner_service.tinynote.service_url
+# }
